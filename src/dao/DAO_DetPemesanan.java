ceeaac827e5493b9b4a5b7c2c99b4d620f2ba1c9
NDORRRRR <24051204191@mhs.unesa.ac.id>
2025-06-12 23:40:28 +0700

    Update DAO_DetPemesanan.java

--- src/dao/DAO_DetPemesanan.java
+++ src/dao/DAO_DetPemesanan.java
@@ -8,6 +8,7 @@ package dao;
 import config.Koneksi;
 import java.util.List;
 import model.Model_DetPemesanan;
+import model.Model_barang;
 import service.Service_DetPemesanan;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -51,50 +52,47 @@ public class DAO_DetPemesanan implements Service_DetPemesanan{
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    
     @Override
     public List<Model_DetPemesanan> getData(String id) {
-    List<Model_DetPemesanan> list = new ArrayList<>();
-    String sql = "SELECT det_psn.no_pesan, det_psn.kode_barang, brg.nama_barang, "
-               + "brg.harga, det_psn.jml_pesan, det_psn.subtotal_pesan, det_psn.status "
-               + "FROM detail_pemesanan det_psn "
-               + "INNER JOIN pemesanan psn ON psn.no_pesan = det_psn.no_pesan "
-               + "INNER JOIN barang brg ON brg.kode_barang = det_psn.kode_barang "
-               + "WHERE det_psn.no_pesan = ? ORDER BY det_psn.no_pesan ASC";
+        List<Model_DetPemesanan> list = new ArrayList<>();
+         String sql = "SELECT det_psn.no_pesan, det_psn.kode_barang, brg.nama_barang, "
+                   + "brg.harga, det_psn.jml_pesan, det_psn.subtotal_pesan, det_psn.status "
+                   + "FROM detail_pemesanan det_psn "
+                   + "INNER JOIN pemesanan psn ON psn.no_pesan = det_psn.no_pesan "
+                   + "INNER JOIN barang brg ON brg.kode_barang = det_psn.kode_barang "
+                   + "WHERE det_psn.no_pesan = ? ORDER BY det_psn.no_pesan ASC";
 
-    try (PreparedStatement st = conn.prepareStatement(sql)) {
-        st.setString(1, id);
-        try (ResultSet rs = st.executeQuery()) {
-            while (rs.next()) {
-                Model_Pemesanan psn = new Model_Pemesanan();
-                Model_Barang brg = new Model_Barang();
-                Model_DetPemesanan det_psn = new Model_DetPemesanan();
-                psn.setNo_pesan(rs.getString("no_pesan"));
+        try (PreparedStatement st = conn.prepareStatement(sql)) {
+            st.setString(1, id);
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    Model_pemesanan psn = new Model_pemesanan();
+                    Model_barang brg = new Model_barang(); // Perbaikan nama kelas
+                    Model_DetPemesanan det_psn = new Model_DetPemesanan();
+                    psn.setNo_pesan(rs.getString("no_pesan"));
 
-                
-                brg.setKode_barang(rs.getString("kode_barang"));
-                brg.setNama_barang(rs.getString("nama_barang"));
-                brg.setHarga(rs.getLong("harga"));
+                    brg.setKode_Barang(rs.getString("kode_barang"));
+                    brg.setNama_barang(rs.getString("nama_barang"));
+                    brg.setHarga(rs.getLong("harga"));
 
-                
-                det_psn.setModel_pemesanan(psn);
-                det_psn.setModel_barang(brg);
-                det_psn.setJml_pesan(rs.getLong("jml_pesan"));
-                det_psn.setSubtotal_pesan(rs.getLong("subtotal_pesan"));
-                det_psn.setStatus(rs.getString("status"));
+                    det_psn.setMod_pesan(psn); // Perbaikan: setMod_pesan bukan setModel_pemesanan
+                    det_psn.setMod_barang(brg); // Perbaikan: setMod_barang bukan setModel_barang
+                    det_psn.setJml_pesan(rs.getLong("jml_pesan"));
+                    det_psn.setSubTotal(rs.getLong("subtotal_pesan")); // Perbaikan: setSubTotal bukan setSubtotal_pesan
+                    det_psn.setStatus(rs.getString("status"));
 
-                list.add(det_psn);
+                    list.add(det_psn);
+                }
             }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
-    } catch (SQLException ex) {
-        ex.printStackTrace();
+        return list;
     }
-    return list;
-}
 
-    
     @Override
     public List<Model_DetPemesanan> pencarian(String id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+}
